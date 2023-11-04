1a5118f082dfe945f7a4aca3a42e16d750a119ac
Jonatan Werpers <jonatan@werpers.com>
2023-11-04 14:16:09 +0100

    Hide the use of token arrays for to_matrix in the api
    --HG--
    branch : refactor/to_matrix

--- Tokens/src/Tokens_test.jl
+++ Tokens/src/Tokens_test.jl
@@ -1,5 +1,6 @@
 using Test
 using Tokens
+using Tokens: _to_matrix
 using SparseArrays
 
 @testset "ScalarToken" begin
@@ -91,46 +92,46 @@ end
 
 @testset "to_matrix" begin
     @testset "vector of linear combinations" begin
-        @test to_matrix(Tokens.LinearCombination[], 0, 0) == spzeros(0,0)
+        @test _to_matrix(Tokens.LinearCombination[], 0, 0) == spzeros(0,0)
 
-        @test to_matrix([1IndexedToken(:v,1)], 1, 1) == sparse(ones(1,1))
-        @test to_matrix([2IndexedToken(:v,1)], 1, 1) == sparse(2ones(1,1))
+        @test _to_matrix([1IndexedToken(:v,1)], 1, 1) == sparse(ones(1,1))
+        @test _to_matrix([2IndexedToken(:v,1)], 1, 1) == sparse(2ones(1,1))
 
         v = ArrayToken(:v, 2)
-        @test to_matrix([v[1]+2v[2], -v[2]], 2, 2) == sparse([
+        @test _to_matrix([v[1]+2v[2], -v[2]], 2, 2) == sparse([
             1 2;
             0 -1;
         ])
 
         v = ArrayToken(:v, 3)
-        @test to_matrix([v[1]+v[2], v[2]+v[3]], 2, 3) == sparse([
+        @test _to_matrix([v[1]+v[2], v[2]+v[3]], 2, 3) == sparse([
             1 1 0;
             0 1 1;
         ])
 
         v = ArrayToken(:v, 2)
-        @test_broken to_matrix([v[1]+v[2], v[2], -v[1]], 3, 2) == sparse([
+        @test_broken _to_matrix([v[1]+v[2], v[2], -v[1]], 3, 2) == sparse([
              1 1;
              0 1;
             -1 0;
         ])
 
         v = ArrayToken(:v, 3)
-        @test_broken to_matrix([v[1], v[2], v[3]], 3, 3) == sparse([
+        @test_broken _to_matrix([v[1], v[2], v[3]], 3, 3) == sparse([
             1 0 0;
             0 1 0;
             0 0 1;
         ])
 
         v = ArrayToken(:v, 3)
-        @test_broken to_matrix([zero(Token), v[2], zero(Token)], 3, 3) == sparse([
+        @test_broken _to_matrix([zero(Token), v[2], zero(Token)], 3, 3) == sparse([
             0 0 0;
             0 1 0;
             0 0 0;
         ])
 
         v = ArrayToken(:v, 3)
-        @test to_matrix([zero(Token), v[2]+v[1], zero(Token)], 3, 3) == sparse([
+        @test _to_matrix([zero(Token), v[2]+v[1], zero(Token)], 3, 3) == sparse([
             0 0 0;
             1 1 0;
             0 0 0;
