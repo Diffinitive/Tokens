5f0966d949d55fb6d8208e632c1b5c3374166912
Jonatan Werpers <jonatan@werpers.com>
2023-11-08 15:07:04 +0100

    Add laplace and alternative formulation of D1
    --HG--
    branch : refactor/operators

--- nb_comparison.jl
+++ nb_comparison.jl
@@ -1,5 +1,5 @@
 ### A Pluto.jl notebook ###
-# v0.19.30
+# v0.19.27
 
 using Markdown
 using InteractiveUtils
@@ -20,7 +20,7 @@ begin
 end
 
 # ╔═╡ 5217dad3-9064-4107-8be9-9ae6784062ea
-function D1(v, h)
+function D1alt(v, h)
 	N = length(v)
 	vₓ = similar(v)
 	
@@ -33,7 +33,56 @@ function D1(v, h)
 	vₓ[N] = (v[N] - v[N-1])/h
 	
 	return vₓ
+end; # Is this faster?
+
+# ╔═╡ ad33d948-ff32-44d9-89d2-5aa560b9a7fc
+function D1(v, i, h)
+	if i == 1
+		return (v[2] - v[1])/h
+	end
+
+	if i == length(v)
+		return (v[end] - v[end-1])/h
+	end
+
+	return (v[i+1] - v[i-1])/2h
 end;
+
+# ╔═╡ 5ef1bcee-49bd-4619-9f3d-8c70fef9cf93
+function D1!(v′, v, h)
+	for i ∈ 1:length(v)
+		v′[i] = @inline D1(v,i,h)
+	end
+end
+
+# ╔═╡ cfb98682-74b8-4819-b856-b63863552513
+# D1(v,h) = [D1(v,i,h) for i ∈ 1:length(v)]
+function D1(v,h)
+	v′ = similar(v)
+	
+	@inline D1!(v′,v,h)
+
+	return v′
+end
+
+# ╔═╡ c33a6b33-17a5-4e16-8f98-ef0082203874
+function laplace(v,i,h)
+   if i == 1 || i == length(v)
+	   return zero(eltype(v))
+   end
+   
+   return (v[i-1]-2v[i]+v[i+1])/h^2
+end
+
+# ╔═╡ 7814eed6-486d-44d5-aa73-3f79128ddc62
+laplace(v,h) = [laplace(v,i,h) for i ∈ 1:length(v)]
+
+# ╔═╡ 14c41a58-c093-4716-96ce-dcd89fd6f08a
+function laplace!(∇²v, v, h)
+	for i ∈ 1:length(v)
+		∇²v[i] = laplace(v,i,h)
+	end
+end
 
 # ╔═╡ d2ee3ced-ac21-4f7e-bc7e-81e277baf533
 md"""
@@ -124,6 +173,12 @@ end
 # ╔═╡ Cell order:
 # ╠═de05a6cf-9742-4240-9b8c-ff6ee8f077b9
 # ╠═5217dad3-9064-4107-8be9-9ae6784062ea
+# ╠═ad33d948-ff32-44d9-89d2-5aa560b9a7fc
+# ╠═cfb98682-74b8-4819-b856-b63863552513
+# ╠═5ef1bcee-49bd-4619-9f3d-8c70fef9cf93
+# ╠═c33a6b33-17a5-4e16-8f98-ef0082203874
+# ╠═7814eed6-486d-44d5-aa73-3f79128ddc62
+# ╠═14c41a58-c093-4716-96ce-dcd89fd6f08a
 # ╟─d2ee3ced-ac21-4f7e-bc7e-81e277baf533
 # ╟─a63b2fe7-fdb8-49dd-9827-bddcac0e98e7
 # ╠═622cb349-7935-44f2-aa5f-57e1fa59b213
